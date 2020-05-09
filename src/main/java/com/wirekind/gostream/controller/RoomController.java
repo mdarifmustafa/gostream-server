eaa3dc186bcfc5b3be9b54e5adff398d37fca509
Mohammad Arif Mustafa <43742651+mohammadarifmustafa@users.noreply.github.com>
2020-05-09 19:48:33 +0530

    fixed rest api controlling bug
    fixed rest api controlling bug

--- src/main/java/com/wirekind/gostream/controller/RoomController.java
+++ src/main/java/com/wirekind/gostream/controller/RoomController.java
@@ -11,10 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.wirekind.gostream.model.Room;
 import com.wirekind.gostream.model.Users;
 
+@RestController
 @RequestMapping("/api")
 public class RoomController {
 
@@ -24,7 +26,7 @@ public class RoomController {
 	@Autowired
 	Users users;
 
-	@RequestMapping(path = "/create-room", method = RequestMethod.GET, produces = "application/json")
+	@RequestMapping(value = "/create-room", method = RequestMethod.GET, produces = "application/json")
 	public Map<String, String> createRoom(@RequestParam(name = "username") String userName) {
 		if (users.registeredUser(userName)) {
 			UUID uuid = UUID.randomUUID();
@@ -41,18 +43,18 @@ public class RoomController {
 		}
 	}
 
-	@RequestMapping(path = "/validate", method = RequestMethod.GET)
+	@RequestMapping(value = "/validate", method = RequestMethod.GET)
 	public boolean validateRoom(@RequestParam(name = "room") String requestedRoom) {
 		return room.validateRoom(requestedRoom);
 	}
 
-	@RequestMapping(path = "/remove", method = RequestMethod.GET)
+	@RequestMapping(value = "/remove", method = RequestMethod.GET)
 	public boolean removeRoom(@RequestParam(name = "room") String requestedRoom) {
 		return room.removeRoom(requestedRoom);
 	}
 
 	@SuppressWarnings("unchecked")
-	@RequestMapping(path = "/remove-rooms", method = RequestMethod.PUT)
+	@RequestMapping(value = "/remove-rooms", method = RequestMethod.PUT)
 	public boolean removeRooms(@RequestParam(name = "rooms") Object requestedRooms) {
 		try {
 			Map<String, List<String>> map = (Map<String, List<String>>) requestedRooms;
@@ -63,12 +65,12 @@ public class RoomController {
 		}
 	}
 
-	@RequestMapping(path = "/available-rooms", method = RequestMethod.GET)
+	@RequestMapping(value = "/available-rooms", method = RequestMethod.GET)
 	public List<String> getAvailableRooms() {
 		return room.getAvailableRooms().stream().map(map -> map.get("address")).collect(Collectors.toList());
 	}
 
-	@RequestMapping(path = "/join-room", method = RequestMethod.GET)
+	@RequestMapping(value = "/join-room", method = RequestMethod.GET)
 	public boolean joinRoom(@RequestParam(name = "userName") String userName,
 			@RequestParam(name = "room") String room) {
 		return users.joinRoom(userName, room);
--- src/main/java/com/wirekind/gostream/controller/UserController.java
+++ src/main/java/com/wirekind/gostream/controller/UserController.java
@@ -9,32 +9,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.wirekind.gostream.model.User;
 import com.wirekind.gostream.model.Users;
 
+@RestController
 @RequestMapping("/api")
 public class UserController {
 
 	@Autowired
 	Users users;
 
-	@RequestMapping(path = "/create-user", method = RequestMethod.POST, consumes = "application/json")
+	@RequestMapping(value = "/create-user", method = RequestMethod.POST, consumes = "application/json")
 	public ResponseEntity<String> createUser(@RequestBody User user) {
 		return users.addUser(user);
 	}
 
-	@RequestMapping(path = "/registered-users", method = RequestMethod.GET, produces = "application/json")
+	@RequestMapping(value = "/registered-users", method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<List<User>> registeredUsers() {
 		return new ResponseEntity<List<User>>(users.getUsers(), HttpStatus.OK);
 	}
 
-	@RequestMapping(path = "/validate-user", method = RequestMethod.GET)
+	@RequestMapping(value = "/validate-user", method = RequestMethod.GET)
 	public Boolean validateRoom(@RequestParam(name = "username") String userName) {
 		return users.registeredUser(userName);
 	}
 
-	@RequestMapping(path = "/remove-user", method = RequestMethod.GET)
+	@RequestMapping(value = "/remove-user", method = RequestMethod.GET)
 	public Boolean removeRoom(@RequestParam(name = "username") String userName) {
 		return users.removeUser(userName);
 	}
